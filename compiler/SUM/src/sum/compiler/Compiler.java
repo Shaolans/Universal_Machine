10c012dae09cb7b154d9932ec1add11a54569689
L-C <L-C@DESKTOP-UKL1B7D>
2018-03-23 21:22:35 +0100

    SUM comments

--- compiler/SUM/src/sum/compiler/Compiler.java
+++ compiler/SUM/src/sum/compiler/Compiler.java
@@ -32,9 +32,16 @@ public class Compiler implements IASTvisitor {
 	private static boolean printtree = false;
 	private static boolean asmtrace = true;
 	private static final int NO_CONTEXT = -1;
+	
+	//numero d'instruction courant
 	private int numInst;
+	
+	//indice de la position de la variable dans le plateau
 	private int indVar;
+	
+	//map de la variable et de la position de la variable dans le plateau
 	private Map<String, Integer> env;
+	
 	private FileOutputStream fos;
 	
 	public Compiler() {
@@ -53,6 +60,7 @@ public class Compiler implements IASTvisitor {
 		
 		this.visit(iast, NO_CONTEXT);
 		
+		//operation d'arret du programme
 		writeOperation(CompilerInstruction.HALT, 0, 0, 0);
 		
 		try {
@@ -63,6 +71,13 @@ public class Compiler implements IASTvisitor {
 		
 	}
 	
+	/**
+	 * Permet d'écrire l'opération en binaire dans le fichier de sortie
+	 * @param op le numéro de l'opération
+	 * @param regA l'indice du registre A
+	 * @param regB l'indice du registre B
+	 * @param regC l'indice du registre C
+	 */
 	public void writeOperation(int op, int regA, int regB, int regC) {
 		if(op == CompilerInstruction.ARRAY_INDEX && asmtrace) System.out.println("REG["+regA+"] = "+"Array[REG["+regB+"][REG["+regC+"]]");
 		if(op == CompilerInstruction.ARRAY_AMEND && asmtrace) System.out.println("Array[REG["+regA+"][REG["+regB+"]] = REG["+regC+"]");
@@ -76,6 +91,7 @@ public class Compiler implements IASTvisitor {
 		operation[1] = 0;
 		operation[2] = (byte)((regA & 0x00000004)==0?0:1);
 		byte val = 0;
+		//on d'interesse au i-eme bit, si il est a 1 alors on ajoute 2^i
 		val += (byte)((regC & 0x00000001)==0?0:1);
 		val += (byte)((regC & 0x00000002)==0?0:2);
 		val += (byte)((regC & 0x00000004)==0?0:4);
@@ -93,6 +109,11 @@ public class Compiler implements IASTvisitor {
 		numInst++;
 	}
 	
+	/**
+	 * Charge dans le registre A la valeur value sur 25 bits
+	 * @param regA l'indice du registre
+	 * @param value la valeur
+	 */
 	public void writeSpecialOperation(int regA, int value) {
 		if(asmtrace) System.out.println("CHARGE REG["+regA+"] = "+value);
 		byte operation[] = new byte[4];
@@ -103,7 +124,7 @@ public class Compiler implements IASTvisitor {
 		val += (byte)((regA & 0x00000001)==0?0:2);
 		val += (byte)((value & 0x01000000)==0?0:1);
 		operation[0] = val;
-		operation[1] = (byte)((value & 0x00FF0000) >>> 16);
+		operation[1] = (byte)((value & 0x00FF0000) >>> 16); //on n'oublie pas de décalé les bits
 		operation[2] = (byte)((value & 0x0000FF00) >>> 8);
 		operation[3] = (byte)(value & 0x000000FF);
 		try {
@@ -114,18 +135,32 @@ public class Compiler implements IASTvisitor {
 		numInst++;
 	}
 	
+	/**
+	 * Récupère une valeur simple situé dans le plateau[i][0] dans le cas d'une variable
+	 * et l'ajoute dans le registre reg
+	 * @param i la position de la variable dans le plateau
+	 * @param reg l'indice du registre
+	 */
 	public void fetchIntoReg(int i, int reg) {
 		writeSpecialOperation(7, i);
 		writeSpecialOperation(6, 0);
 		writeOperation(CompilerInstruction.ARRAY_INDEX, reg, 7, 6);
 	}
 	
+	/**
+	 * Ajoute dans le plateau[i][0] la valeur dans le registre d'indice reg
+	 * @param i l'indice dans le plateau
+	 * @param reg l'indice du registre
+	 */
 	public void putIntoArray(int i, int reg) {
 		writeSpecialOperation(7, i);
 		writeSpecialOperation(6, 0);
 		writeOperation(CompilerInstruction.ARRAY_AMEND, 7, 6, reg);
 	}
 	
+	/**
+	 * Alloue un plateau de taille 1 pour une variable
+	 */
 	public void allocateVar() {
 		writeSpecialOperation(7, 1);
 		writeOperation(CompilerInstruction.ALLOCATION, 0, 7, 7);
@@ -152,6 +187,8 @@ public class Compiler implements IASTvisitor {
 		allocateVar();
 		iast.getCondition().accept(this, condctx);
 		
+		//suivant la valeur de la condition, on saute soit a l'adresse du then ou alors a l'adresse du else
+		//les blocs de then et else sont fixés, ce qui permet de calculer directement les adresses de saut
 		writeSpecialOperation(1, numInst+8);
 		writeSpecialOperation(0, numInst+8+DEFAULT_SIZE-1);
 		fetchIntoReg(condctx, 2);
@@ -159,16 +196,18 @@ public class Compiler implements IASTvisitor {
 		writeSpecialOperation(1, 0);
 		writeOperation(CompilerInstruction.LOAD_PROG, 0, 1, 0);
 		
-		
+		//instructions du then
 		int start = numInst;
 		for(IASTstatement istmt: iast.getConsequence()) {
 			istmt.accept(this, NO_CONTEXT);
 		}
 		
+		//saut hors du if
 		writeSpecialOperation(1, 0);
 		writeSpecialOperation(0, start+2*DEFAULT_SIZE);
 		writeOperation(CompilerInstruction.LOAD_PROG, 0, 1, 0);
 		
+		//on remplit le reste du bloc avec des NOP
 		boolean asmtrace_atpre = asmtrace;
 		asmtrace = false;
 		if(asmtrace_atpre) System.out.println("FILL REST OF ALTERNATIVE (CONS) WITH NOP");
@@ -177,10 +216,12 @@ public class Compiler implements IASTvisitor {
 		}
 		asmtrace = asmtrace_atpre;
 		
+		//insutrctions du else
 		for(IASTstatement istmt: iast.getAlternative()) {
 			istmt.accept(this, NO_CONTEXT);
 		}
 		
+		//on remplit le reste du bloc avec des NOP
 		asmtrace = false;
 		if(asmtrace_atpre) System.out.println("FILL REST OF ALTERNATIVE (ALT) WITH NOP");
 		while(numInst < start+2*DEFAULT_SIZE) {
@@ -193,10 +234,12 @@ public class Compiler implements IASTvisitor {
 	public void visit(IASTbinding iast, int context) {
 		if(printtree) System.out.println("Binding");
 		
+		//allocation d'une variable
 		int varcontext = indVar++;
-		env.put(iast.getName(), varcontext);
 		allocateVar();
+		env.put(iast.getName(), varcontext);
 		
+		//si une expression est avec la declaration, on stock la valeur dans le plateau[varcontext][0]
 		if(iast.getExpression() != null) iast.getExpression().accept(this, varcontext);
 	}
 
@@ -312,6 +355,14 @@ public class Compiler implements IASTvisitor {
 		iast.getArg2().accept(this, contextarg2);
 		fetchIntoReg(contextarg1, 0);
 		fetchIntoReg(contextarg2, 1);
+		
+		//on peut remarquer que soit a un entier
+		//a+NAND(a,a)+1 = 0 <=> a+~(a&a)+1 = 0
+		//car NAND(a,a) <=> NOT(a)
+		//par conséquent a et NOT(a) on des bits opposés
+		//si on effectue l'addition on a donc tout les bits a 1
+		//et si on effectue une addition a +1 on dépasse la capacité on retombe sur 0
+		//Donc a+NAND(b,b)+1 = 0 ssi a = b
 		writeOperation(CompilerInstruction.NOT_AND, 2, 1, 1);
 		writeSpecialOperation(3, 1);
 		writeOperation(CompilerInstruction.ADD, 2, 2, 3);
@@ -335,13 +386,15 @@ public class Compiler implements IASTvisitor {
 		iast.getArg2().accept(this, contextarg2);
 		fetchIntoReg(contextarg1, 1);
 		fetchIntoReg(contextarg2, 2);
+		
+		//si reg[1]/reg[2] = 0 alors reg[1] < reg[2] car c'est une division entiere 
 		writeOperation(CompilerInstruction.DIV, 3, 1, 2);
 		writeSpecialOperation(0, 0);
 		writeSpecialOperation(4, 1);
 		writeOperation(CompilerInstruction.COND_MOV, 0, 4, 3);
 		
 		
-		
+		//on verifie que reg[1] != reg[2]
 		writeOperation(CompilerInstruction.NOT_AND, 3, 2, 2);
 		writeSpecialOperation(4, 1);
 		writeOperation(CompilerInstruction.ADD, 3, 3, 4);
@@ -407,6 +460,9 @@ public class Compiler implements IASTvisitor {
 		fetchIntoReg(contextarg2, 2);
 		writeSpecialOperation(0, 0);
 		writeSpecialOperation(3, 1);
+		//reg[1] et reg[2] contiennent le resultat de membre gauche et droit de OR
+		//reg[0] contient 0, si reg[1] ou reg[2] contiennent la valeur != 0 alors
+		//on met dans reg[0] la valeur 1, donc ceci est bien un OR
 		writeOperation(CompilerInstruction.COND_MOV, 0, 3, 1);
 		writeOperation(CompilerInstruction.COND_MOV, 0, 3, 2);
 		putIntoArray(context, 0);
@@ -431,6 +487,10 @@ public class Compiler implements IASTvisitor {
 		writeSpecialOperation(5, 0);
 		writeSpecialOperation(1, 0);
 		writeSpecialOperation(0, 0);
+		//reg[2] et reg[3] contiennent le resultat du membre gauche et droit de AND
+		//reg[1] = 0 si reg[2] != 0 alors reg[1] = 1
+		//reg[0] = 0 si reg[3] != 0 alors reg[0] = reg[1]
+		//donc si on a 0 AND 1, reg[0] = 0 etc
 		writeOperation(CompilerInstruction.COND_MOV, 1, 4, 2);
 		writeOperation(CompilerInstruction.COND_MOV, 0, 1, 3);
 		
@@ -448,6 +508,7 @@ public class Compiler implements IASTvisitor {
 		fetchIntoReg(argcontext, 2);
 		writeSpecialOperation(0, 1);
 		writeSpecialOperation(1, 0);
+		//si le resultat est != 0 alors on met 1 sinon 0
 		writeOperation(CompilerInstruction.COND_MOV, 0, 1, 2);
 		putIntoArray(context, 0);
 	}
--- compiler/SUM/src/sum/test/TestCompiler.java
+++ compiler/SUM/src/sum/test/TestCompiler.java
@@ -1,9 +1,5 @@
 package sum.test;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
 
 import sum.ast.ast.ASTfactory;
 import sum.interfaces.iast.IASTfactory;
@@ -11,7 +7,7 @@ import sum.interfaces.iast.IASTprogram;
 import sum.parser.SUMParser;
 
 public class TestCompiler {
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) {
 		IASTfactory factory = new ASTfactory();
 		SUMParser sp = new SUMParser(factory);
 		for(int i = 0; i < 18; i++) {
@@ -19,18 +15,6 @@ public class TestCompiler {
 			sum.compiler.Compiler c = new sum.compiler.Compiler();
 			c.compile(p,"out\\test"+i+".um");
 		}
-		/*
-		try {
-			FileInputStream is = new FileInputStream(new File("out\\test2.um"));
-			byte b[] = new byte[12];
-			is.read(b);
-			for(int i = 0; i < 12; i++) {
-				System.out.println(b[i]);
-			}
-		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}*/
 		
 	}
 }
